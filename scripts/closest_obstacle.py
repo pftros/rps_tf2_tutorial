bd6128e3f54cad779f06046fa56edb464d73c24c
Damjan Miklic <damjan@romb-technologies.hr>
2020-12-11 19:59:52 +0100

    Broadcasting a fixed transform.

--- scripts/closest_obstacle.py
+++ scripts/closest_obstacle.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import rospy
+import tf2_ros
+
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import TransformStamped
+
+from tf_conversions import transformations
 
 class ObstacleBroadcaster:
     """
@@ -10,10 +15,24 @@ class ObstacleBroadcaster:
     """
     def __init__(self):
         self.sub = rospy.Subscriber('scan', LaserScan, self.scan_callback)
-
+        self.tf_bcaster = tf2_ros.TransformBroadcaster()
+        # Initialize the constant transform fields
+        self.tf_laser_obst = TransformStamped()
+        self.tf_laser_obst.header.frame_id = 'base_laser_link'
+        self.tf_laser_obst.child_frame_id = 'obstacle'
+        self.tf_laser_obst.transform.translation.z = 0.0
+        
     def scan_callback(self, scan):
         """ This is where all the action happens! """
-        print('min: {}'.format(min(scan.ranges)))
+        self.tf_laser_obst.header.stamp = rospy.Time.now()
+        self.tf_laser_obst.transform.translation.x = 1
+        self.tf_laser_obst.transform.translation.y = 0.5
+        q = transformations.quaternion_from_euler(0, 0, 0.707)
+        self.tf_laser_obst.transform.rotation.x = q[0]
+        self.tf_laser_obst.transform.rotation.y = q[1]
+        self.tf_laser_obst.transform.rotation.z = q[2]
+        self.tf_laser_obst.transform.rotation.w = q[3]
+        self.tf_bcaster.sendTransform(self.tf_laser_obst)
 
     def run(self):
         while not rospy.is_shutdown():
