a1ec02a1a59019bcb5bf4b0226b5d4f6d918e49b
Damjan Miklic <damjan@romb-technologies.hr>
2020-12-12 20:04:26 +0100

    Broadcasting the obstacle tansform in the Odom frame.

--- scripts/closest_obstacle.py
+++ scripts/closest_obstacle.py
@@ -5,9 +5,10 @@ import rospy
 import tf2_ros
 
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import TransformStamped, Quaternion
+from geometry_msgs.msg import TransformStamped, Quaternion, PoseStamped
 
 from tf_conversions import transformations
+from tf2_geometry_msgs import do_transform_pose
 from math import sin, cos
 
 class ObstacleBroadcaster:
@@ -17,24 +18,40 @@ class ObstacleBroadcaster:
     def __init__(self):
         self.sub = rospy.Subscriber('scan', LaserScan, self.scan_callback)
         self.tf_bcaster = tf2_ros.TransformBroadcaster()
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
         # Initialize the constant transform fields
-        self.tf_laser_obst = TransformStamped()
-        self.tf_laser_obst.header.frame_id = 'base_laser_link'
-        self.tf_laser_obst.child_frame_id = 'obstacle'
-        self.tf_laser_obst.transform.translation.z = 0.0
+        self.tf_odom_obst = TransformStamped()
+        self.tf_odom_obst.header.frame_id = 'odom'
+        self.tf_odom_obst.child_frame_id = 'obstacle'
+        self.tf_odom_obst.transform.translation.z = 0.0
         
     def scan_callback(self, scan):
         """ This is where all the action happens! """
-        self.tf_laser_obst.header.stamp = rospy.Time.now()
+        self.tf_odom_obst.header.stamp = rospy.Time.now()
         range_obs = min(scan.ranges)
         idx_obs = scan.ranges.index(range_obs)
         angle_obs = scan.angle_min + idx_obs*scan.angle_increment
-        trans = self.tf_laser_obst.transform.translation
-        trans.x = range_obs*cos(angle_obs)
-        trans.y = range_obs*sin(angle_obs)
+        pose_obs = PoseStamped()
+        pose_obs.pose.position.x = range_obs*cos(angle_obs)
+        pose_obs.pose.position.y = range_obs*sin(angle_obs)
         q = transformations.quaternion_from_euler(0, 0, angle_obs)
-        self.tf_laser_obst.transform.rotation = Quaternion(*q)
-        self.tf_bcaster.sendTransform(self.tf_laser_obst)
+        pose_obs.pose.orientation = Quaternion(*q)
+        try:
+            buf = self.tf_buffer
+            tf_odom_laser = buf.lookup_transform('odom',
+                                                 'base_laser_link',
+                                                 rospy.Time())
+            pose_odom_obs = do_transform_pose(pose_obs, tf_odom_laser)
+            pose_odom_obs = pose_odom_obs.pose
+            tf = self.tf_odom_obst.transform
+            tf.translation = pose_odom_obs.position
+            tf.rotation = pose_odom_obs.orientation
+            self.tf_bcaster.sendTransform(self.tf_odom_obst)
+        except (tf2_ros.LookupException,
+                tf2_ros.ConnectivityException,
+                tf2_ros.ExtrapolationException) as ex:
+            rospy.logwarn(ex)
 
     def run(self):
         while not rospy.is_shutdown():
