f0170ff62d30d532faf27dbb14d7d4cc07c224ec
Damjan Miklic <damjan@romb-technologies.hr>
2025-11-09 22:20:50 +0000

    Detect landmarks.

--- landmark_localization/landmark_localization/landmark_localization.py
+++ landmark_localization/landmark_localization/landmark_localization.py
@@ -2,7 +2,10 @@ import rclpy
 from rclpy.node import Node
 
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Point
+from visualization_msgs.msg import Marker
 
+from math import sin, cos
 
 class LandmarkLocalization(Node):
 
@@ -16,7 +19,27 @@ class LandmarkLocalization(Node):
         self.subscription  # prevent unused variable warning
 
     def scan_callback(self, scan):
-        self.get_logger().info(f'{scan.header}')
+        # Idenfiy landmarks
+        landmarks = {'prism': {'x': None,
+                               'y': None},
+                     'cylinder': {'x': None,
+                                  'y': None}}
+        # We assume that cylinder readings precede prism readings.
+        # We approximate landmark poses with a single reading.
+        for idx, r in enumerate(scan.ranges):
+            if r > scan.range_min and r < scan.range_max:
+                angle = scan.angle_min + idx * scan.angle_increment
+                landmarks['cylinder']['x'] = r * cos(angle)
+                landmarks['cylinder']['y'] = r * sin(angle)
+                break
+        for idx, r in enumerate(reversed(scan.ranges)):
+            if r > scan.range_min and r < scan.range_max:
+                angle = scan.angle_max - idx * scan.angle_increment
+                landmarks['prism']['x'] = r * cos(angle)
+                landmarks['prism']['y'] = r * sin(angle)
+                break
+
+        self.get_logger().info(f'{landmarks}')
 
 
 def main(args=None):
